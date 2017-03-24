36a51cc656a19a5be19e2451bb18a5f731b72d0e
Jon Gold <hello@designedbygold.com>
2017-03-24 12:01:41 -0700

    move logic to index

---
+++ index.js
@@ -0,0 +1,22 @@
+const exec = require('child_process').exec;
+const pictureTube = require('picture-tube')
+const chalk = require('chalk');
+const tube = pictureTube({ cols: 48 });
+tube.pipe(process.stdout);
+const fs = require('fs');
+
+module.exports = (args) => {
+  fs.createReadStream('./ryan.png').pipe(tube);
+
+  console.log(`You can do it ${process.env.USER || '' }!`);
+  const cmd = `yarn ${args.join(' ')}`;
+  console.log(chalk.white(cmd));
+  exec(cmd, (e, stdout, stderr) => {
+    if (e instanceof Error) {
+        return console.error(chalk.red(e.message));
+    }
+    console.log(stdout);
+    console.log(stderr);
+  });
+
+}
